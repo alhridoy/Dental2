b58ecc9e79d4bbbefad01fd198d87284db049bf5
Al-Iqram Elahee Hridoy <aliqramalaheehridoy@gmail.com>
2018-07-30 19:24:13 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+(function ($) {
+
+ 
+ $('.clients .owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        smartSpeed: 650
+    });
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+ 
+})(jQuery);
+
+
+
+
+
